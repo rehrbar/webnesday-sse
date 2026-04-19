1fe34ac34a1e2f2db6c5b2b163513f2de2761e2a
Roman Ehrbar <software@romanehrbar.ch>
2026-04-19 15:24:07 +0200

    feat: submit votes

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,32 @@ import express from 'express';
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+// Enable json requests.
 app.use(bodyParser.json());
 
+// Stores a list of all connected clients.
 let clients: Array<{
     id: string,
     response: express.Response
 }> = [];
-function eventsHandler(request: express.Request, response: express.Response) {
+
+// The shared data, could be in a database or memory store.
+const validVotes = [1, 2, 3, 4, 5];
+const votes = new Map<string, { votes: number}>();
+
+function sendClientCountToAll() {
+    clients.forEach(client => client.response.write(`event: clients\ndata: ${clients.length}\n\n`));
+}
+
+function sendVotesToAll() {
+  // TODO fix serialization of map.
+    clients.forEach(client => client.response.write(`event: votes\ndata: ${JSON.stringify(votes)}\n\n`))
+}
+
+// Endpoint for clients to subscribe for updates.
+app.get('/events', (request: express.Request, response: express.Response) => {
+  // Client will keep the connection open with these headers.
   const headers = {
     'Content-Type': 'text/event-stream',
     'Connection': 'keep-alive',
@@ -17,9 +36,10 @@ function eventsHandler(request: express.Request, response: express.Response) {
   };
   response.writeHead(200, headers);
 
-  // TODO send server status
+  // At this point, the missed events could be fed using an offset indicated by Last-Event-ID.
   response.write(`data: ${JSON.stringify({status: "joined"})}\n\n`);
 
+  // Store the client so updates can be broadcasted.
   const clientId = self.crypto.randomUUID();
 
   const newClient = {
@@ -28,34 +48,40 @@ function eventsHandler(request: express.Request, response: express.Response) {
   };
 
   clients.push(newClient);
+  sendClientCountToAll();
 
+  // Handle client disconnecting and remove them from the list receiving updates.
   request.on('close', () => {
     console.log(`${clientId} Connection closed`);
     clients = clients.filter(client => client.id !== clientId);
+    sendClientCountToAll();
   });
-}
+});
 
-app.get('/events', eventsHandler);
+// Endpoint to submit a new vote.
+app.post('/votes', (request: express.Request, response: express.Response) => {
+    const { vote } = request.body;
+    if(validVotes.includes(vote)) {
+      // Update the vote count.
+      const t = votes.get(vote) || {votes: 0};
+      t.votes += 1;
+      votes.set(vote, t);
 
-function sendEventsToAll(newFact:string) {
-    clients.forEach(client => client.response.write(`data: ${JSON.stringify({newFact})}\n\n`))
-}
+      response.sendStatus(201);
 
-async function addFact(request: express.Request, response: express.Response) {
-    const newFact = request.body;
-    response.json(newFact);
-    // TODO update server status
-    return sendEventsToAll(newFact);
-}
+      sendVotesToAll();
+    }
+});
 
-app.post('/messages', addFact)
+// Endpoint to delete all stored votes.
+app.delete('/votes', (request: express.Request, response: express.Response) => {
+    votes.clear();
+    sendVotesToAll();
+    response.sendStatus(200);
+});
 
+// Serve static files, e.g. the slides or the UI for this demo.
 app.use(express.static('public'));
-
-setInterval(() => {
-    console.log("clients connected", clients.length)
-    clients.forEach(client => client.response.write(`event: clients\ndata: ${clients.length}\n\n`))
-}, 1000);
 
 app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
